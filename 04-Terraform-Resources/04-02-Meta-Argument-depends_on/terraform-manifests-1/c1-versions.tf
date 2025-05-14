c6eccbc455606d102bbd9aaae7e5ca80bf794d80
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-05-14 16:40:39 +0330

    add section 04-02-meta-arguments

---
+++ 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests-1/c1-versions.tf
@@ -0,0 +1,18 @@
+# Terraform Settings Block
+terraform {
+  required_version = "~> 1.8.0"
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "2.8.1"
+    }
+  }
+}
+
+# Provider Block
+provider "vsphere" {
+  user                 = "administrator@vsphere.local"
+  password             = "P@ssw0rd"
+  vsphere_server       = "1.2.3.4"
+  allow_unverified_ssl = true
+}
---
+++ 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests-1/c2-saleh-vm.tf
@@ -0,0 +1,57 @@
+# Data Block
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter-lab"
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "vSAN-lab"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "ds" {
+  name          = "vsanDatastore"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+# Resource Block
+// added in this section
+
+resource "vsphere_folder" "saleh_folder" {
+  path          = "saleh_folder"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "saleh_vm" {
+  name             = "saleh-vm"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.ds.id
+
+  num_cpus = 4
+  memory   = 4096
+
+  guest_id = "otherGuest"
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = 10
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+  wait_for_guest_net_timeout  = 0
+  wait_for_guest_net_routable = false
+  wait_for_guest_ip_timeout   = 0
+// added in this section
+  folder     = vsphere_folder.saleh_folder.path
+  depends_on = [vsphere_folder.saleh_folder]
+}
