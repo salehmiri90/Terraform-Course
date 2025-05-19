d6670efa4ce33c2f2405b4c0a8c1b05ce5073a72
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-05-19 08:42:35 +0330

    add 04-02-manifest3

---
+++ 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests-3-foreach/c2-saleh-vm.tf
@@ -0,0 +1,56 @@
+# Data Block
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter-lab"
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "vSAN-lab"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "ds" {
+  name          = "vsanDatastore"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+  
+# Resource Block
+resource "vsphere_virtual_machine" "saleh_vm" {
+  // added in this section
+  for_each = {
+    "vm-a" = 5
+    "vm-b" = 10
+    "vm-c" = 15
+    "vm-d" = 20
+    "vm-e" = 25
+  }
+  name     = "saleh-${each.key}"
+  //
+  num_cpus = 2
+  memory   = 2048
+
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.ds.id
+  guest_id = "otherGuest"
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
+  }
+
+  disk {
+    // added in this section
+    label            = "saleh-${each.key}-disk0"
+    size             = each.value
+    //
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+  wait_for_guest_net_timeout  = 0
+  wait_for_guest_net_routable = false
+  wait_for_guest_ip_timeout   = 0
+}
