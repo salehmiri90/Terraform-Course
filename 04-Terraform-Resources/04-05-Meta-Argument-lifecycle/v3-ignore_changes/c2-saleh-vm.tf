33d43d882db7a29c7247e400cec02c154f6a89d0
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-05-20 07:43:32 +0330

    add readme 04-05

--- 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v3-ignore_changes/c2-saleh-vm.tf
+++ 04-Terraform-Resources/04-05-Meta-Argument-lifecycle/v3-ignore_changes/c2-saleh-vm.tf
@@ -1,17 +1,50 @@
-# Create EC2 Instance
-resource "aws_instance" "web" {
-  ami = "ami-0915bcb5fa77e4892" # Amazon Linux
-  instance_type = "t2.micro"
-  tags = {
-    "Name" = "web-3"
-  }
-/*
-  lifecycle {
-    ignore_changes = [
-      # Ignore changes to tags, e.g. because a management agent
-      # updates these based on some ruleset managed elsewhere.
-      tags,
-    ]
-  }*/
+# Data Block
+data "vsphere_datacenter" "dc" {
+  name = "Datacenter-lab"
 }
 
+data "vsphere_compute_cluster" "cluster" {
+  name          = "vSAN-lab"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "ds" {
+  name          = "vsanDatastore" 
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+  
+# Resource Block
+resource "vsphere_virtual_machine" "saleh_vm" {
+  name     = "saleh-lifecycle3"
+  num_cpus = 1
+  memory   = 1024
+
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.ds.id
+  guest_id = "otherGuest"
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = "vmxnet3"
+  }
+
+  disk {
+    label            = "disk0"
+    size             = 10
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+  wait_for_guest_net_timeout  = 0
+  wait_for_guest_net_routable = false
+  wait_for_guest_ip_timeout   = 0
+  // added in this section
+  lifecycle {
+    ignore_changes = [name]
+  }
+  //
+}
