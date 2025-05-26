230da43c05ad42f59a275515fcd01ecc09e0578a
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-05-26 18:07:04 +0330

    add 05-01 v01

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v01-Input-Variables-Basic/c2-variables.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v01-Input-Variables-Basic/c2-variables.tf
@@ -2,16 +2,17 @@
 variable "vm_name" {
   description = "the name of virtual machine"
   type        = string
+  #default     = "saleh-vm"
 }
 
 variable "vm_cpu" {
   description = "cpu core counts"
   type        = number
-  default     = 2
+  #default     = 2
 }
 
 variable "vm_ram" {
   description = "ram value based on MegaBytes"
   type        = number
-  default     = 1024
+  #default     = 1024
 }
