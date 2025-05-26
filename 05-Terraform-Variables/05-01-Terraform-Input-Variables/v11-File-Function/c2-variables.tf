a68b0e853ad222dcc53959a05dcef700eff7deea
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-05-26 20:21:15 +0330

    add 05-01 v11

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v11-File-Function/c2-variables.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v11-File-Function/c2-variables.tf
@@ -1,24 +1,5 @@
-# Input Variables
-variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
-  type = string
-  default = "us-east-1"
-}
-
-variable "ec2_ami_id" {
-  description = "AMI ID"
-  type = string  
-  default = "ami-0915bcb5fa77e4892"
-}
-
-variable "ec2_instance_count" {
-  description = "EC2 Instance Count"
-  type = number
-  default = 1
-}
-
-variable "ec2_instance_type" {
-  description = "EC2 Instance Type"
-  type = string
-  default = "t3.micro"
+variable "public_key" {
+  description = "SSH Public Key"
+  type        = string
+  default     = file("${path.module}/id_rsa.pub")
 }
