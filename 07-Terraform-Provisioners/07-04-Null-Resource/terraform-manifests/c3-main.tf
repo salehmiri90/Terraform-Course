c5beeba02a3a7358a2dac3727a3d9d4f476e39c2
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-06-08 10:33:08 +0330

    add 07-04

---
+++ 07-Terraform-Provisioners/07-04-Null-Resource/terraform-manifests/c3-main.tf
@@ -0,0 +1,50 @@
+resource "vsphere_virtual_machine" "this" {
+  name                   = var.vm_name
+  resource_pool_id       = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id           = data.vsphere_datastore.datastore.id
+  num_cpus               = var.num_cpus
+  memory                 = var.memory * 1024
+  firmware               = "efi"
+  guest_id               = data.vsphere_virtual_machine.template.guest_id
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+  disk {
+    label            = "${var.vm_name}-disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    unit_number      = 0
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.uuid
+    timeout       = 600
+
+    customize {
+      linux_options {
+        host_name = var.vm_os_hostname
+        domain    = var.vm_os_domain_name
+      }
+
+      network_interface {
+        ipv4_address = var.ipv4_address
+        ipv4_netmask = var.ipv4_netmask
+      }
+
+      ipv4_gateway    = var.ipv4_gateway
+    }
+  }
+}
+
+resource "null_resource" "log_vm_creation" {
+  provisioner "local-exec" {
+    command     = "echo 'VM ${var.vm_name} created on Today!' >> ./logs/vm_creation.log"
+    interpreter = ["/bin/bash", "-c"]
+  }
+
+  depends_on = [
+    vsphere_virtual_machine.this
+  ]
+}
---
+++ 07-Terraform-Provisioners/07-04-Null-Resource/terraform-manifests/c4-datasource.tf
@@ -0,0 +1,24 @@
+# Data Block
+data "vsphere_datacenter" "dc" {
+  name = var.datacenter_name
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.datastore_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.network_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.cluster_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
