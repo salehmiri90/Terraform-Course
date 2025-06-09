aa850781afffaf6f292df601d37ccbf9fa229306
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-06-09 10:15:53 +0330

    add 10

--- 10-Terraform-Debug/terraform-manifests/c3-main.tf
+++ 10-Terraform-Debug/terraform-manifests/c3-main.tf
@@ -14,30 +14,3 @@ module "saleh_vm" {
   eagerly_scrub    = var.eagerly_scrub
   thin_provisioned = var.thin_provisioned
 }
-
-
-#---
-# resource "vsphere_virtual_machine" "saleh_vm" {
-#   name     = var.vm_name
-#   num_cpus = var.num_cpus
-#   memory   = var.memory
-
-#   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-#   datastore_id     = data.vsphere_datastore.ds.id
-#   guest_id = "otherGuest"
-
-#   network_interface {
-#     network_id   = data.vsphere_network.network.id
-#     adapter_type = "vmxnet3"
-#   }
-
-#   disk {
-#     label            = "disk0"
-#     size             = 5
-#     eagerly_scrub    = false
-#     thin_provisioned = true
-#   }
-#   wait_for_guest_net_timeout  = 0
-#   wait_for_guest_net_routable = false
-#   wait_for_guest_ip_timeout   = 0
-# }
