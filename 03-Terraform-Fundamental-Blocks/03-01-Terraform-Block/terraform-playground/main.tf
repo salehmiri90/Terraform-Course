3da3d72c637351be4cf19f0d19e7ae7193edd127
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-07-13 19:35:02 +0330

    module-03-01 edit playground

--- 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-playground/main.tf
+++ 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-playground/main.tf
@@ -4,6 +4,7 @@
 2. Create a terraform block include terraform version
 3. In the existing terraform block, findout the proper provider name and version to run below code.
 4. Run terraform workflow to do the task
+5. Check the new added files under the .terraform directories
 */
 # Place to write your code here:
 
