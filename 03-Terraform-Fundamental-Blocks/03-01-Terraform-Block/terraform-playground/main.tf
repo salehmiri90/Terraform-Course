b807534d9e9eb73167dbaeb65c45d9e532e020f4
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-07-13 19:30:04 +0330

    module-03-01 add playground

--- 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-playground/main.tf
+++ 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-playground/main.tf
@@ -1,3 +1,11 @@
+# TASK for HOME
+/*
+1. Find out your local terraform version. write it here: 
+2. Create a terraform block include terraform version
+3. In the existing terraform block, findout the proper provider name and version to run below code.
+*/
+# Place to write your code here:
+
 resource "local_file" "pet" { 
     filename = "/root/pets.txt" 
     content = "We love pets!"
