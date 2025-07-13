c319197eb772d53bbadb8269259f777fec8223b6
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-07-13 20:44:41 +0330

    module-03-02 add playground

---
+++ 03-Terraform-Fundamental-Blocks/03-02-Provider-Block/terraform-playground/.terraform.lock.hcl
@@ -0,0 +1,15 @@
+# Task for Home
+/*
+This is sample file based on choosen local version on the main.tf file.
+*/
+
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.5.1"
+  constraints = "2.5.1"
+  hashes = [
+    "h1:8oTPe2VUL6E2d3OcrvqyjI4Nn/Y/UEQN26WLk5O/B0g=",
+  ]
+}
---
+++ 03-Terraform-Fundamental-Blocks/03-02-Provider-Block/terraform-playground/main.tf
@@ -0,0 +1,22 @@
+# Task for Home
+/*
+Play with provider version base on the information on terraform webiste, 
+then 
+run terraform init and check the .terraform.lock.hcl file content
+*/
+
+# terraform block
+terraform {
+  required_version = "~> 1.8.0"
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
+}
+# resource block
+resource "local_file" "pet" { 
+    filename = "/root/pets.txt" 
+    content = "We love pets!"
+}
