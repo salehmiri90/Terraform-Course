b6c03e67177dafa6dbd84ca3f17ac89236ac8d05
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-07-28 18:41:05 +0330

    add 04-01 playground

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-playground/main.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-playground/main.tf
@@ -19,7 +19,7 @@ resource "random_pet" "demo1" {
   length = 2
 }
 
-resource "local_file" "pet" {
+resource "local_file" "saleh" {
   content  = "The pet name is ${random_pet.demo1.id}"
   filename = "/${path.module}/pets.txt"
 }
