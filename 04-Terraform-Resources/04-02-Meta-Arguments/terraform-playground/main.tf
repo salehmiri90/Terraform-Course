9a7839d3ee9b12d69d63a46ede051c8c4979f452
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-07-28 18:33:29 +0330

    add 04-02 playground

---
+++ 04-Terraform-Resources/04-02-Meta-Arguments/terraform-playground/main.tf
@@ -0,0 +1,31 @@
+# Task for Home
+
+# terraform block
+terraform {
+  required_version = "~> 1.8.0"
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "= 3.7.2"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "= 2.5.3"
+    }
+  }
+}
+# resource block
+provider "random" {}
+
+resource "random_pet" "mypet" {
+  length    = 2
+  separator = "-"
+}
+
+resource "local_file" "mypet" {
+  // When random_pet mypet resource is finished, then this resource will run
+  depends_on = [random_pet.mypet] 
+
+  filename = "${path.module}/pet_name.txt"
+  content  = "Generated pet name: ${random_pet.mypet.id}"
+}
