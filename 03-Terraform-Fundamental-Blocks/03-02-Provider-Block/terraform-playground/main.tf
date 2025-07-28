fc3b932e202e855e9a34c9422357328e7f3c44eb
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-07-28 19:52:50 +0330

    add 03-02 playground

--- 03-Terraform-Fundamental-Blocks/03-02-Provider-Block/terraform-playground/main.tf
+++ 03-Terraform-Fundamental-Blocks/03-02-Provider-Block/terraform-playground/main.tf
@@ -11,12 +11,12 @@ terraform {
   required_providers {
     local = {
       source = "hashicorp/local"
-      version = "2.5.1"
+      version = ">= 2.5.0"
     }
   }
 }
 # resource block
 resource "local_file" "saleh" { 
-    filename = "/${path.module}/pets.txt" 
+    filename = "pets.txt" 
     content = "We love pets!"
 }
