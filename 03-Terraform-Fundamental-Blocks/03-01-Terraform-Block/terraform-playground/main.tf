7ff35168f45076597f36e0ef4c5264802146f833
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-07-28 18:38:32 +0330

    add 03-01 playground

--- 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-playground/main.tf
+++ 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-playground/main.tf
@@ -6,7 +6,7 @@
 4. Run terraform workflow to do the task
 5. Check the new added files under the .terraform directories
 */
-# Place to write your code here:
+# Place to write your code here :
 
 resource "local_file" "saleh" { 
     filename = "/${path.module}/pets.txt" 
