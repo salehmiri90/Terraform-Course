1187681ad08c0bd3a29893b5a99501b140454c92
Seyed Saleh Miri <salehmiri90@gmail.com>
2025-07-29 09:06:59 +0330

    add 04-04 playground

--- 04-Terraform-Resources/04-04-Meta-Argument-for_each/terraform-playground-toset/main.tf
+++ 04-Terraform-Resources/04-04-Meta-Argument-for_each/terraform-playground-toset/main.tf
@@ -11,16 +11,19 @@ terraform {
   }
 }
 
+# Variable Block
+variable "vm_names" {
+  default = [
+    "tosetvm-01", 
+    "tosetvm-02", 
+    "tosetvm-03"
+  ]
+}
+
 # resource block
 resource "local_file" "saleh" {
-  for_each = {
-    "vm-a" = 10
-    "vm-b" = 20
-    "vm-c" = 30
-    "vm-d" = 40
-    "vm-e" = 50
-  }
+  for_each = toset(var.vm_names)
   filename = "saleh-${each.key}"
-  content  = "Disk capacity for this ${each.key} is ${each.value} GB"
+  content  = "Disk capacity for this ${each.value} is 15 GB"
 }
 // open the created file and check the content carefully
