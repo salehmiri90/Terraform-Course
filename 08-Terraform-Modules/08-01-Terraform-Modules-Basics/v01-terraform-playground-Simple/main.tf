2e97af6ec7c0b86e278e20fad9de4cc5346563d2
saleh miri <salehmiri90@gmail.com>
2025-08-11 01:37:31 -0700

    add 08-01 v01-terraform-playground

---
+++ 08-Terraform-Modules/08-01-Terraform-Modules-Basics/v01-terraform-playground-Simple/main.tf
@@ -0,0 +1,12 @@
+// non-module terraform code
+
+resource "local_file" "pet" {
+  filename = var.filename
+  content  = var.content
+}
+
+resource "random_pet" "my-pet" {
+  prefix    = var.prefix
+  separator = var.separator
+  length    = var.length
+}
---
+++ 08-Terraform-Modules/08-01-Terraform-Modules-Basics/v01-terraform-playground-Simple/variables.tf
@@ -0,0 +1,23 @@
+variable "filename" {
+  default     = "pets.txt"
+  type        = string
+  description = "the path of local file"
+  sensitive   = true
+}
+variable "content" {
+  default     = "I love pets!"
+  type        = string
+  description = "the content of the file"
+}
+variable "prefix" {
+  default     = "Mrs"
+  type        = string
+  description = "the prefix to be set"
+}
+variable "separator" {
+  default = "."
+}
+variable "length" {
+  default = 2
+  type    = number
+}
