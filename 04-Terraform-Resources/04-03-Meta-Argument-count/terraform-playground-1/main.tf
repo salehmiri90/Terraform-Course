4dfe5ef261396ffa343f522a427d17ff1c7f2a35
saleh miri <salehmiri90@gmail.com>
2025-08-27 21:59:25 +0330

    edit 04-03 section

--- 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-playground-1/main.tf
+++ 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-playground-1/main.tf
@@ -1,19 +1,5 @@
-# Task for Home
+# Lets have fun on Linux!
 
-# terraform block
-terraform {
-  required_version = "~> 1.8.0"
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = ">= 2.5.0"
-    }
-    random = {
-      source = "hashicorp/random"
-      version = ">= 3.7.0"
-    }
-  }
-}
 # variables block
 // first apply with below and check the result.
 variable "filename" {
--- 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-playground-2/main.tf
+++ 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-playground-2/main.tf
@@ -1,20 +1,6 @@
-# Task for Home
+# Lets have fun on Linux!
 
-# terraform block
-terraform {
-  required_version = "~> 1.8.0"
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = ">= 2.5.0"
-    }
-    random = {
-      source = "hashicorp/random"
-      version = ">= 3.7.0"
-    }
-  }
-}
-
+# variable block
 variable "filename" {
   default = [
     "playground-04-03-pets.txt",
