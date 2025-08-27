fced8060a6433e5d116da5b110b8dc637a76bb50
saleh miri <salehmiri90@gmail.com>
2025-08-27 21:57:16 +0330

    edit 04-01 section

---
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-playground/versions.tf
@@ -0,0 +1,15 @@
+# Lets have fun on Linux!
+# terraform block
+terraform {
+  required_version = "~> 1.8.0"
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = ">= 2.5.0"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.7.0"
+    }
+  }
+}
